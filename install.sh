604562d3462ae4bb784d3fcd66a6be08242e6285
wildRaccoon <chukvm@gmail.com>
2017-07-14 15:09:17 +0300

    paper theme

--- install.sh
+++ install.sh
@@ -19,6 +19,12 @@ sudo add-apt-repository ppa:numix/ppa
 sudo apt-get update
 sudo apt-get install numix-gtk-theme numix-icon-theme-circle
 
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update
+sudo apt-get install paper-icon-theme
+sudo apt-get install paper-cursor-theme
+sudo apt-get install paper-gtk-theme
+
 sudo apt install ubuntu-restricted-extras libavcodec-extra libdvd-pkg
 
 sudo add-apt-repository ppa:paulo-miguel-dias/mesa
@@ -27,3 +33,9 @@ sudo apt-get update
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-7 g++-7
+
+wget https://download.01.org/gfx/RPM-GPG-GROUP-KEY-ilg
+sudo apt-key add RPM-GPG-GROUP-KEY-ilg
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install intel-linux-graphics-update-tool
