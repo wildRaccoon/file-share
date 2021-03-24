e3abaf2054c49e2188b7a40c88e1777ff46d37a9
wildRaccoon <chukvm@gmail.com>
2021-03-24 09:18:42 +0200

    add scripts

---
+++ cluster/reboot.sh
@@ -0,0 +1,4 @@
+ssh -T ubuntu@rpi01 "sudo reboot"
+ssh -T ubuntu@rpi02 "sudo reboot"
+ssh -T ubuntu@rpi03 "sudo reboot"
+ssh -T ubuntu@rpi04 "sudo reboot"
---
+++ cluster/shutdown.sh
@@ -0,0 +1,4 @@
+ssh -T ubuntu@rpi01 "sudo shutdown -h now"
+ssh -T ubuntu@rpi02 "sudo shutdown -h now"
+ssh -T ubuntu@rpi03 "sudo shutdown -h now"
+ssh -T ubuntu@rpi04 "sudo shutdown -h now"
