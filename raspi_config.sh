5840b31e962432a058158a762bfba3cc7a41188a
wildRaccoon <chukvm@gmail.com>
2021-04-13 07:59:37 +0300

    update file

--- raspi_config.sh
+++ raspi_config.sh
@@ -1,4 +1,4 @@
-wget https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20210212_all.deb -P /tmp
+wget https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20210212_all.deb
 sudo apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
 # Auto install dependancies on eg. ubuntu server on RPI
 sudo apt-get install -fy
